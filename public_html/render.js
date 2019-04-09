d299c197a70e256afaeb20b37c9cb320f0c57da2
Malakai <mediacenteruser@Media-Center-Kit-10.local>
2019-04-09 08:49:07 -0500

    Update render.js

--- public_html/render.js
+++ public_html/render.js
@@ -113,7 +113,7 @@ function positionPing(position){
         var found = new futchure( storedHash );
         found.render();
       }
-      futch.render();
+      console.log("searching....");
     });
   });
 
